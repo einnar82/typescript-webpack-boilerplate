2c6ca6e027cd4637cd221af223d31c33ce6db95f
Rannie <einnar82@gmail.com>
2020-12-28 18:16:32 +0800

    Added config for dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,11 @@
 const path = require("path");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: {
+    app: "./src/index.ts",
+  },
   module: {
     rules: [
       {
@@ -15,11 +19,13 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  devServer: {
-    contentBase: "./dist",
-    publicPath: "/",
-    hot: true,
-  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    // new CleanWebpackPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: "Production",
+    // }),
+  ],
   output: {
     publicPath: "dist",
     filename: "bundle.js",
